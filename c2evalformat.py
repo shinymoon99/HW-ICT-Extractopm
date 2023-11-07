9aa4056442a48e769cb3e2a420efa4809678adb3
shinymoon99 <2686288339@qq.com>
2023-11-07 19:48:43 +0800

    dataset error fixed

--- c2evalformat.py
+++ c2evalformat.py
@@ -78,11 +78,12 @@ def build_data(text,label_locs,event_type):
 datas = []
 inputtexts = []
 labelposes = []
-format_dataset = []
+
 names = ["点选","配置","查看","创建"]
 file_names = ["click","configure","view","create"]
 for i in range(4):
-    with open(f"./datasets/selected_4/train_txt/{file_names[i]}_SL.txt","r",encoding="utf-8") as f:
+    format_dataset = []
+    with open(f"./datasets/selected_4/test_txt/{file_names[i]}_SL_test.txt","r",encoding="utf-8") as f:
         inputtexts =f.readlines()
 
     sourcetexts = removeQuotes(inputtexts)
@@ -92,5 +93,5 @@ for i in range(4):
         labelposes.append(label_pos)
         output_data =build_data(s,label_pos,e_type)
         format_dataset.append(output_data)
-    with open(f"./datasets/selected_4/train_json/{file_names[i]}.json","w") as f1:
+    with open(f"./datasets/selected_4/test_json/{file_names[i]}.json","w") as f1:
         json.dump(format_dataset,f1)
--- utils/util.py
+++ utils/util.py
@@ -14,6 +14,21 @@ def combine_json_files_in_folder(folder_path):
 
     return combined_data
 
+def json_to_lines(input_file, output_file):
+    try:
+        with open(input_file, 'r') as json_file, open(output_file, 'w') as output:
+            data = json.load(json_file)
+            for item in data:
+                output.write(json.dumps(item) + '\n')
+        print(f"Conversion complete. Lines written to {output_file}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+# Usage example
+# input_file = 'input.json'
+# output_file = 'output.txt'
+# json_to_lines(input_file, output_file)
+
 # Example usage:
 # input_folder = "./path_to_folder_containing_json_files"
 # combined_data = combine_json_files_in_folder(input_folder)
